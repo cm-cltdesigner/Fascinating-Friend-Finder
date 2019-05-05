18ef009c941876a7564bbed12dd57639d254af1a
courtney manery <courtneymmanery@gmail.com>
2019-05-05 14:06:39 -0400

    First Commit

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+[{
+"name": "Carrie Bradshaw",
+"photo":"https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAzNAAAAJDk4YmFiODRmLTRmYTUtNGU1NC1hMThmLTM1OTExNTcwYTdlYQ.jpg",
+"answers[]":["5","2","3","4","2","4","1","3","5","4"]},
+{
+"name": "Charlotte York",
+"photo":"https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/Sarah_Jessica_Parker_at_Miami_Rhapsody_30th_Anniversary_Celebration.jpg/440px-Sarah_Jessica_Parker_at_Miami_Rhapsody_30th_Anniversary_Celebration.jpg",
+"answers[]":["5","5","4","2","3","1","4","5","2","5"]},
+{
+"name": "Samantha Jones",
+"photo":"https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Kim_Cattrall_Meet_Monica_Velour_2011.jpg/440px-Kim_Cattrall_Meet_Monica_Velour_2011.jpg",
+"answers[]":["2","4","1","2","2","1","2","5","5","3"]},
+{
+"name": "Miranda Hobbes",
+"photo":"https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Kristindavis1999.jpg/440px-Kristindavis1999.jpg",
+"answers[]":["1","5","5","1","4","3","2","2","5","3"]},
+{
+"name": "Jack Berger",
+"photo":"https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/RonLivingstonMay10.jpg/440px-RonLivingstonMay10.jpg",
+"answers[]":["4","3","2","3","4","1","3","5","5","2"]},
+]
