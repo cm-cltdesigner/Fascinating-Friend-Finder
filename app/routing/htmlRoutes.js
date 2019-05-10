7540f429d271904edadc3645a477acde4d024180
courtney manery <courtneymmanery@gmail.com>
2019-05-09 22:11:38 -0400

    debugging

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 // NOTE: THIS IS THE HTML ROUTES
-=======
-// NOTE: THIS FOR HMTL ROUTES
->>>>>>> 5b4f46a0aa9a19bb9bef8503e6446eb9851dcba5
 
 module.exports = function(app, path) {
 	app.get("/", function(req, res) {
--- server.js
+++ server.js
@@ -7,8 +7,8 @@ var fs = require('fs');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 
 // Static Files
