01ed2df69e3747105bce2370177d700bc33eb6ec
courtney manery <courtneymmanery@gmail.com>
2019-05-09 21:17:34 -0400

    debugging server

--- server.js
+++ server.js
@@ -3,11 +3,7 @@ var express = require('express');
 var path = require('path');
 var fs = require('fs');
 
-<<<<<<< HEAD
 // Sets up the Express App
-=======
-// Sets Express App
->>>>>>> 5b4f46a0aa9a19bb9bef8503e6446eb9851dcba5
 var app = express();
 var PORT = process.env.PORT || 3000;
 
@@ -25,10 +21,5 @@ require('./app/routing/htmlRoutes.js')(app, path);
 
 
 app.listen(PORT, function() {
-<<<<<<< HEAD
     console.log("Hey, I\'m listening... on port " + PORT);
 })
-=======
-    console.log("I\'m listening... on port " + PORT);
-})
->>>>>>> 5b4f46a0aa9a19bb9bef8503e6446eb9851dcba5
